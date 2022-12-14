f6b318d22b5d8b2741576add4e5c3f8567e6a911
vijaykumardumpeti <vijaydumpeti70@gmail.com>
2022-12-14 05:56:39 +0000

    digital timer app

---
+++ src/components/DigitalTimer/index.js
@@ -0,0 +1,125 @@
+import {Component} from 'react'
+import './index.css'
+
+export default class DigitalTimer extends Component {
+  state = {
+    timeInMinutes: 25,
+    timeInSeconds: '00',
+    isStartButtonClicked: false,
+  }
+
+  startButtonClicked = () => {
+    this.setState(prevState => ({
+      isStartButtonClicked: !prevState.isStartButtonClicked,
+    }))
+  }
+
+  onResetButton = () => {
+    this.setState({
+      timeInMinutes: 25,
+      timeInSeconds: '00',
+    })
+  }
+
+  onDecrement = () => {
+    const {timeInMinutes, isStartButtonClicked} = this.state
+    if (timeInMinutes > 25 && isStartButtonClicked === false) {
+      this.setState(prevState => ({
+        timeInMinutes: prevState.timeInMinutes - 1,
+      }))
+    }
+  }
+
+  onIncrement = () => {
+    const {isStartButtonClicked} = this.state
+    if (isStartButtonClicked === false) {
+      this.setState(prevState => ({
+        timeInMinutes: prevState.timeInMinutes + 1,
+      }))
+    }
+  }
+
+  render() {
+    const {timeInMinutes, timeInSeconds, isStartButtonClicked} = this.state
+    const url = isStartButtonClicked
+      ? 'https://assets.ccbp.in/frontend/react-js/pause-icon-img.png'
+      : 'https://assets.ccbp.in/frontend/react-js/play-icon-img.png'
+
+    const altText = isStartButtonClicked ? 'pause icon' : 'play icon'
+
+    const text = isStartButtonClicked ? 'Pause' : 'Start'
+    const timerText = isStartButtonClicked ? 'Running' : 'Paused'
+
+    return (
+      <div className="bg-container">
+        <h1>Digital Timer</h1>
+        <div className="container">
+          <div className="timer-container">
+            <div className="timer-small-container">
+              <div className="pause-para">
+                <h1>
+                  {timeInMinutes}:{timeInSeconds}
+                </h1>
+                <p>{timerText}</p>
+              </div>
+            </div>
+          </div>
+          <div className="buttons-container">
+            <div className="pause-reset-button-container">
+              <div className="pause-button-container">
+                <button
+                  onClick={this.startButtonClicked}
+                  className="pause-play-buttons"
+                  type="button"
+                >
+                  <div className="pause-botton-container">
+                    <img className="pause-play-icons" alt={altText} src={url} />
+
+                    <h1>{text}</h1>
+                  </div>
+                </button>
+              </div>
+              <div className="reset-button-container">
+                <button
+                  onClick={this.onResetButton}
+                  className="pause-play-buttons"
+                  type="button"
+                >
+                  <img
+                    className="pause-play-icons"
+                    alt="reset icon"
+                    src="https://assets.ccbp.in/frontend/react-js/reset-icon-img.png"
+                  />
+                </button>
+
+                <div>
+                  <h1>Reset</h1>
+                </div>
+              </div>
+            </div>
+            <div className="plus-minus-container-1">
+              <p>Set Timer limit</p>
+              <div className="plus-minus-container-2">
+                <button
+                  onClick={this.onDecrement}
+                  className="plus-minus-button"
+                  type="button"
+                >
+                  -
+                </button>
+                <p className="twenty-five">{timeInMinutes}</p>
+                <button
+                  onClick={this.onIncrement}
+                  className="plus-minus-button"
+                  type="button"
+                >
+                  +
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
